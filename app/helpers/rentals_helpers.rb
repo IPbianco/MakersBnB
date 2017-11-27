3b3e0793fb4fea1e19f16847b3f9d849d9aa83c6
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-27 17:24:52 +0000

    adds check dates helper

--- app/helpers/rentals_helpers.rb
+++ app/helpers/rentals_helpers.rb
@@ -1,5 +1,12 @@
-# require 'json'
+require 'date'
 
 def get_rentals(rental_class: Rental)
   rental_class.all.to_json
 end
+
+def check_dates(id, start, finish, rental_class: Rental)
+  (Array((start + 1)...finish) & 
+   rental_class.first(id).bookings.map(&:date)).empty?
+end
+  
+
