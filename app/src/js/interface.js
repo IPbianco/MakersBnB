24e7565a8cb9f22bb0eaf7c931db936d8a2c67bb
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-28 17:22:31 +0000

    Linked heroku database to interface

--- app/src/js/interface.js
+++ app/src/js/interface.js
@@ -1,26 +1,34 @@
 
-  // $.get()'/rentals', function (rentals) { };
 
+var rental
 $(document).ready(function(){
-  var rental = [{"id":1,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/c1/45/b4/c145b4fa6322048fce85cba6b2bcf0ed.jpg"},{"id":2,"price":1000,"address":"nowhere","img-link":"https://i.pinimg.com/564x/52/71/f6/5271f60f50441b0aebbc0204d8176354.jpg"}
-, {"id":1,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/75/f7/d0/75f7d06a433fc20bea883fe6203ba70e.jpg"}, {"id":1,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/75/f7/d0/75f7d06a433fc20bea883fe6203ba70e.jpg"}
-, {"id":4, "price":45, "address":"place", "img-link":"https://i.pinimg.com/564x/c7/c9/fb/c7c9fbdde85aaf2ce15cac4d0c45e9b6.jpg"}]
 
-  $(function () {
-    // Grab the template script
-    var theTemplateScript = $("#property-template").html();
+    $.getJSON('https://obscure-spire-75698.herokuapp.com/rentals', function(rentals) {
+     rental = rentals
 
-   // Compile the template
-    var theTemplate = Handlebars.compile(theTemplateScript);
+     $(function() {
+       var theTemplateScript = $("#property-template").html();
+       var theTemplate = Handlebars.compile(theTemplateScript);
 
-   // This is the default context, which is passed to the template
-    var context = {
-      rentals: rental
-    };
+       var context = {
+         rentals: rental
+       };
 
-    var theCompiledHtml = theTemplate(context);
+       var theCompiledHtml = theTemplate(context);
 
 
-    $(document.body).append(theCompiledHtml);
-  });
+       $(document.body).append(theCompiledHtml);
+     });
+   });
+
+$(document).on('click', 'input#button', function(clickEvent) {
+  console.log(clickEvent.target.value);
+})
+
+//   var rental = [{"id":1,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/c1/45/b4/c145b4fa6322048fce85cba6b2bcf0ed.jpg"},{"id":2,"price":1000,"address":"nowhere","img-link":"https://i.pinimg.com/564x/52/71/f6/5271f60f50441b0aebbc0204d8176354.jpg"}
+// , {"id":3,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/75/f7/d0/75f7d06a433fc20bea883fe6203ba70e.jpg"}, {"id":4,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/75/f7/d0/75f7d06a433fc20bea883fe6203ba70e.jpg"}
+// , {"id":5, "price":45, "address":"place", "img-link":"https://i.pinimg.com/564x/c7/c9/fb/c7c9fbdde85aaf2ce15cac4d0c45e9b6.jpg"}]
+
+
+
 });
