7d19b67b4b50958e8b0375ea5df4f25342bfc5fa
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-28 16:03:25 +0000

    sends 200 response with post routes; does not use global content type

--- app/controllers/rentals.rb
+++ app/controllers/rentals.rb
@@ -1,15 +1,16 @@
 class App < Sinatra::Base
-  before { content_type :json }
-
   get '/rentals' do
+    content_type :json
     rentals_to_json
   end
 
   get '/bookings' do
+    content_type :json
     bookings_to_json
   end
 
   get '/bookings/validate/:id' do |id|
+    content_type :json
     start, finish = parse_dates(params)
     available?(id, start, finish)
   end
@@ -17,5 +18,6 @@ class App < Sinatra::Base
   post '/bookings/:id' do |id|
     start, finish = parse_dates(params)
     book(id, start, finish)
+    200
   end
 end
