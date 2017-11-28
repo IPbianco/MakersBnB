33a78202292d4830350ab3f384c161194a1d4e18
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-11-28 13:42:02 +0000

    Updated pictures in handlebars instead of hardcoded

--- app/src/js/interface.js
+++ app/src/js/interface.js
@@ -2,7 +2,9 @@
   // $.get()'/rentals', function (rentals) { };
 
 $(document).ready(function(){
-  var rental = [{"id":1,"price":0,"address":"nowhere"},{"id":2,"price":1000,"address":"nowhere"}]
+  var rental = [{"id":1,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/c1/45/b4/c145b4fa6322048fce85cba6b2bcf0ed.jpg"},{"id":2,"price":1000,"address":"nowhere","img-link":"https://i.pinimg.com/564x/52/71/f6/5271f60f50441b0aebbc0204d8176354.jpg"}
+, {"id":1,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/75/f7/d0/75f7d06a433fc20bea883fe6203ba70e.jpg"}, {"id":1,"price":0,"address":"nowhere", "img-link":"https://i.pinimg.com/564x/75/f7/d0/75f7d06a433fc20bea883fe6203ba70e.jpg"}
+, {"id":4, "price":45, "address":"place", "img-link":"https://i.pinimg.com/564x/c7/c9/fb/c7c9fbdde85aaf2ce15cac4d0c45e9b6.jpg"}]
 
   $(function () {
     // Grab the template script
