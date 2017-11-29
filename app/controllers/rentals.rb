6c1fafbdd98b07afa18dfa536e85cccd0b11cb31
Eustaquio122 <tiagofsousa@gmail.com>
2017-11-29 13:53:40 +0000

    Returning 200 done

--- app/controllers/rentals.rb
+++ app/controllers/rentals.rb
@@ -26,6 +26,6 @@ class App < Sinatra::Base
 
   post '/bookings/:id' do |id|
     start, finish = parse_dates(params)
-    book(id, start, finish) ? 200 : halt(402)
+    (book(id, start, finish) && 200) || halt(402)
   end
 end
