283391dbf8ffe0f7e611ac3853345de3be7b6d0d
Eustaquio122 <tiagofsousa@gmail.com>
2017-11-29 14:41:05 +0000

    Another test with 8/10 Ignacio Odds

--- app/controllers/rentals.rb
+++ app/controllers/rentals.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   get '/bookings/validate/:id' do |id|
     content_type :json
     start, finish = parse_dates(params)
-    available?(id, start, finish)
+    available?(id, start, finish).to_json
   end
 
   post '/bookings/:id' do |id|
--- app/helpers/rentals_helpers.rb
+++ app/helpers/rentals_helpers.rb
@@ -6,12 +6,12 @@ end
 
 def available?(id, start, finish, rental_class: Rental)
   rental = rental_class.first(id: id)
-  availability_to_json(rental.available?((start..finish)))
+  rental.available?((start..finish))
 end
 
-def availability_to_json(state)
-  { available: state }.to_json
-end
+# def availability_to_json(state)
+#   { available: state }.to_json
+# end
 
 def parse_dates(params, date_class: Date)
   [params[:start], params[:finish]].map { |date| date_class.parse(date) }
