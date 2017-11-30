e799f3f83472332bea86d0d49a77a45a6fb4fdd1
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-30 21:41:13 +0000

    changes rental to_json methods to use Rental.to_hash

--- app/helpers/rentals_helpers.rb
+++ app/helpers/rentals_helpers.rb
@@ -1,21 +1,17 @@
 require 'date'
 
 def rental_to_json(id, rental_class: Rental)
-  rental_class.get(id).to_hash.to_json
+  rental_class.get(id).to_h.to_json
 end
 
 def rentals_to_json(rental_class: Rental)
-  rental_class.all.map(&:to_hash).to_json
+  rental_class.all.map(&:to_h).to_json
 end
 
 def available?(id, start, finish, rental_class: Rental)
   rental = rental_class.first(id: id)
   rental.available?((start..finish))
 end
-
-# def availability_to_json(state)
-#   { available: state }.to_json
-# end
 
 def parse_dates(params, date_class: Date)
   [params[:start], params[:finish]].map { |date| date_class.parse(date) }
