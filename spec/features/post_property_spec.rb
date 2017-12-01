a25c1eb5cde3a7399b916a8a5e66e4505784840e
Eustaquio122 <tiagofsousa@gmail.com>
2017-12-01 14:56:36 +0000

    Updates post rental page specs

--- spec/features/post_property_spec.rb
+++ spec/features/post_property_spec.rb
@@ -2,21 +2,25 @@ require_relative '../helpers/feature_helpers.rb'
 
 feature 'Property Page', js: true do
  scenario 'Shows input for address' do
+    sign_up_and_sign_in
     visit '/rentals/create'
     expect(page).to have_field("address")
   end
 
   scenario 'Shows input for price' do
+    sign_up_and_sign_in
     visit '/rentals/create'
     expect(page).to have_field("price")
   end
 
   scenario 'Shows input for image' do
+    sign_up_and_sign_in
     visit '/rentals/create'
     expect(page).to have_field("image")
   end
 
   scenario 'Shows Submit! button' do
+    sign_up_and_sign_in
     visit '/rentals/create'
     expect(page).to have_button("Submit!")
   end
